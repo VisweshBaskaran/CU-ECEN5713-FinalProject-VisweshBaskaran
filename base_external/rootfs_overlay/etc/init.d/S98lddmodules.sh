773620b5953d84e401c333ac068a5f5e156f5b28
Viswesh <viswesh@Ubuntu.myguest.virtualbox.org>
2023-10-31 20:43:31 -0600

    added start stop for aesdchar

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -4,12 +4,14 @@ case "$1" in
         echo "Starting modules "
         module_load faulty
         scull_load
+        /usr/bin/aesdchar_load
         modprobe hello
         ;;
     stop)
         echo "Stopping modules "
         module_unload faulty
 	scull_unload
+	/usr/bin/aesdchar_unload
         rmmod hello
         ;;
       *)
