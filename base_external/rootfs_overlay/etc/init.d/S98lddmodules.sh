7224d950f22d84ed32cada4421dcd74bfb3f9dea
Viswesh <viswesh@Ubuntu.myguest.virtualbox.org>
2023-10-31 20:46:48 -0600

    removed usr/bin in S988lddmodules.sh

--- base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
+++ base_external/rootfs_overlay/etc/init.d/S98lddmodules.sh
@@ -4,14 +4,14 @@ case "$1" in
         echo "Starting modules "
         module_load faulty
         scull_load
-        /usr/bin/aesdchar_load
+        aesdchar_load
         modprobe hello
         ;;
     stop)
         echo "Stopping modules "
         module_unload faulty
 	scull_unload
-	/usr/bin/aesdchar_unload
+	aesdchar_unload
         rmmod hello
         ;;
       *)
